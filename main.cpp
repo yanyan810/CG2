8b10bd82b77dc832f21ad53cbb09ca593b796a66
yanyan0315 <k024g0064@g.neec.ac.jp>
2025-04-16 14:16:37 +0900

    利用するアダプタや機能レベルなどが表示できるようになった

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <Windows.h>
 #include <cstdint>
+#include <string>
+#include <format>
 #include <filesystem>
-#include<fstream>//ファイルに書いたり読んだりするライブラリ
-#include <chrono>//時間を扱うライブラリ
+#include <fstream>
+#include <chrono>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#include <locale>
+#include <codecvt>
 
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
 
 //クライアント領域サイズ
 const int32_t kClientWidth = 1280;
@@ -28,11 +37,34 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-void Log(std::ostream& os, const std::string& message) {
-	//ログの出力
-	os << message << std::endl;
+void Log(const std::string& message) {
 	OutputDebugStringA(message.c_str());
 }
+
+// std::wstring -> std::string の変換
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return {};
+	}
+
+	int size_needed = WideCharToMultiByte(CP_UTF8, 0, str.c_str(), -1, nullptr, 0, nullptr, nullptr);
+	std::string result(size_needed - 1, 0); // -1 to exclude null terminator
+	WideCharToMultiByte(CP_UTF8, 0, str.c_str(), -1, &result[0], size_needed, nullptr, nullptr);
+	return result;
+}
+
+// std::string -> std::wstring の変換
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return {};
+	}
+
+	int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
+	std::wstring result(size_needed - 1, 0); // -1 to exclude null terminator
+	MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, &result[0], size_needed);
+	return result;
+}
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -74,25 +106,77 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ShowWindow(hwnd, SW_SHOW);
 
 	MSG msg{};
-   
-	//ログのディレクトリを用意
-	std::filesystem::create_directories("logs");
 
-	//現在時刻を取得(UTC時刻)
+	//ログのディレクトリを用意
+	std::filesystem::create_directory("logs");
+
+	//現在時刻を取得(UTI時刻)
 	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 
-	//ログファイルの名前にコンマ何秒はいらないので、削って秒にする
-	std::chrono::time_point<std::chrono::system_clock,std::chrono::seconds>
-		nowSeconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
+	//ログファイルの名前にコンマ何秒はいらないので削って秒にする
+	std::chrono::time_point <std::chrono::system_clock,std::chrono::seconds>
+		nowSecond = std::chrono::time_point_cast<std::chrono::seconds>(now);
 
 	//日本時間(PCの設定時間)に変換
-	std::chrono::zoned_time localTime{ std::chrono::current_zone(), nowSeconds };
-	//formatお使って年月日_時分秒の文字列に変換
-	std::string dateString = std::format("{:%Y%m%d_%H%M%S}", localTime);
+	std::chrono::zoned_time localTime{ std::chrono::current_zone(), nowSecond };
+
+	//formatを使って年月日_時分秒の文字列に変換
+	std::string dataStrigng = std::format("{:%Y%m%d_%H%M%S}", localTime);
 	//時刻を使ってファイル名を決定
-	std::string logFilePath = std::string("logs/") + dateString + ".log";
+	std::string localFilePath = std::string("logs/") + dataStrigng + ".log";
 	//ファイルを作って書き込み準備
-	std::ofstream logStream(logFilePath);
+	std::ofstream logStream(localFilePath);
+
+	//DXGIファクトリーの生成
+	IDXGIFactory7* dxgiFactory = nullptr;
+
+	//HRESULTWindows系のエラーコードであり、
+	// 関数が成功したかどうかをSUCCEEDEDマクロで判定できる
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+	//初期化の根本的なエラーが出た場合はプログラムが間違っているか、どうにもできない場合が多いのでasserにしておく
+	assert(SUCCEEDED(hr));
+
+	//使用するアダプタ用の変数,最初にnullptrを入れておく
+	IDXGIAdapter4* useAdapter = nullptr;
+	//良い順にアダプタを頼む
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i,
+		DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) !=
+		DXGI_ERROR_NOT_FOUND; i++) {
+		//アダプターの情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));//取得できないのは一大事
+		//ソフトウェアのアダプタでなければ採用
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+			//採用したアダプタの情報をログに出力。wstringの方なので注意
+			Log(ConvertString(std::format(L"Use Adapter: {}\n", adapterDesc.Description)));
+			break;
+		}
+		useAdapter = nullptr;//ソフトウェアアダプタの場合は見なかったことにする
+	}
+	//適切なアダプタが見つからなかったので起動できない
+	assert(useAdapter != nullptr);
+
+	//D3D12Deviceの生成
+	ID3D12Device* device = nullptr;
+	//機能レベルとログ出力用の文字列
+	D3D_FEATURE_LEVEL featureLevels[] = {
+		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+	};
+	const char* featureLevelStrings[] = {"12.2","12.1","12.0"};
+	//高い順に生成できるか試していく
+	for (size_t i = 0; i < _countof(featureLevels); i++) {
+		//採用したアダプターでデバイス生成
+		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+		//生成できたのでログ出力を行ってループを抜ける
+		if (SUCCEEDED(hr)) {
+			Log(std::format("Use Feature Level: {}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+	//デバイスの精がうまくいかなかった場合
+	assert(device != nullptr);
+	Log("Complete create D3D12Device!!!\n");//初期化のログを出す
 
 	//ウィンドウボタンのxボタンが押されえるまでループ
 	while (msg.message != WM_QUIT) {
