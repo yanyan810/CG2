bb9336c7f85a732f79c6831edbe77ee5044aea87
yanyan0315 <k024g0064@g.neec.ac.jp>
2025-04-20 22:36:11 +0900

    産さっ系の描画完了

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,8 +1,3 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
-{
-	return pos;
-}
-
 struct VertexShaderOutput{
     float4 position : SV_POSITION;
 };
@@ -19,13 +14,3 @@ VertexShaderOutput main(VertexShaderInput input)
     return output;
 }
 
-struct PixelSharderOutput{
-    float4 color : SV_TARGET0;
-};
-
-PixelSharderOutput main(VertexShaderOutput input)
-{
-    PixelSharderOutput output;
-    output.color = float4(1.0f, 1.0f, 1.0f, 1.0f); // Red color
-    return output;
-}
